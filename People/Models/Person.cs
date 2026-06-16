5fc70d2c612192031634206e812aba2bfae3173d
shamov-kn <cocostean@gmail.com>
2026-06-16 16:34:38 +0700

    Добавьте файлы проекта.

---
+++ People/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace People.Models;
+
+[Table("people")]
+public class Person
+{
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
+    [MaxLength(250), Unique]
+    public string Name { get; set; }
+}
